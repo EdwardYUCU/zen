71cce81a23edde48887c0352ac907f1c3574ec5d
Xiuyuan <xiuyuanyu86@gmail.com>
2024-05-25 17:53:06 +0800

    Add a argparser using clap in lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    /// The file to search from
+    pub filename: String,
+
+    /// The number of the result to show
+    #[arg(short, long)]
+    pub number: i32,
+}
