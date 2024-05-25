be2c919d87dd9cdd89e1766ea27c860f219e2ea6
Xiuyuan <xiuyuanyu86@gmail.com>
2024-05-25 22:55:58 +0800

    More help message

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 
-/// A tool to show the word info in a file
+/// A tool to show the word location in a file
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 pub struct Cli {
@@ -8,7 +8,6 @@ pub struct Cli {
     pub filename: String,
 
     /// The number of the result to show
-    /// sort the result in decending order a show the first n entries.
     #[arg(short, long)]
     pub number: Option(i32),
 }
